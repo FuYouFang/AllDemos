d00f716f756dfb0845a026bc2c3d88131db118b2
fuyoufang <fuyoufang@qq.com>
2015-05-27 21:16:25 +0800

    初始化了新浪微博

---
+++ G01-新浪微博/G01-新浪微博/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  G01-新浪微博
+//
+//  Created by fuyoufang on 15/5/27.
+//  Copyright (c) 2015年 fuyoufang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ G01-新浪微博/G01-新浪微博/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  G01-新浪微博
+//
+//  Created by fuyoufang on 15/5/27.
+//  Copyright (c) 2015年 fuyoufang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
